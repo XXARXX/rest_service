c3ac001836939d9791469832c7e22748b2ec7f07
AR <xxarxx0@gmail.com>
2023-10-21 15:39:39 +0300

    Change load function

--- src/config_ext.py
+++ src/config_ext.py
@@ -6,10 +6,16 @@ class ConfigExtension:
             self.init_app(app)
     
     def init_app(self, app, config_path=None):
+        config = None
         if config_path is None:
-            self.base_dir = load_base_dir()
+            config = load_config()
         else:
-            self.base_dir = load_base_dir(config_path)
+            config = load_config(config_path)
+        
+        if 'base_dir' not in config:
+            raise ConfigError("'base_dir not found in config")
+        
+        self.base_dir = config['base_dir']
 
         if not Path(self.base_dir).is_dir():
             raise ValueError("'%s' does not exist or is not directory" % self.base_dir)
